c542b637c7a17c5ad5eba21a103e2b0300eadf2c
arif-rizal1122 <rizallarif32@gmail.com>
2025-05-12 08:11:49 +0800

    setup project and user

---
+++ src/main/java/com/bank/banking/entity/User.java
@@ -0,0 +1,64 @@
+package com.bank.banking.entity;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    private String firstName;
+
+    private String lastName;
+
+    private String otherName;
+
+    private String gender;
+
+    private String address;
+
+    private String stateOfOrigin;
+
+    private String accountNumber;
+
+    private BigDecimal accountBalance;
+
+    private String email;
+    
+    private String alternativePhoneNumber;
+
+    private String status;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+
+}
